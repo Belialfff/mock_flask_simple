33000168b4297a7c4002ff717c1f628ebbc0fe41
Belialfff <120930349+Belialfff@users.noreply.github.com>
2023-04-26 22:06:28 +0300

    Update mock.py

--- mock.py
+++ mock.py
@@ -6,22 +6,23 @@ import string
 import random
 
 app = Flask(__name__)
-app.config['JSON_SORT_KEYS'] = False
+app.config['JSON_SORT_KEYS'] = False # выключаем сортировку ключей по алфавиту
 
-# обработчик запросов по методам GET и POST и PATH /api/json/
+# обработчик запросов по методу POST и PATH /api/json/
 @app.route('/api/json/<id>', methods=['POST'])
 def json_post(id):
-
-    request_data = request.get_json()
-    friends = request_data["friends"]
-    guid = uuid.uuid1()   # для POST запроса парсим каждое поле, запиысывая его в отдельные переменные
-    debitBalance = request_data["debitBalance"]
-    creditBalance = request_data["creditBalance"]
-    phone = request_data ["phone"]
-    registered = request_data["registered"]
-    is_active = bool(request_data)
-
-
+    # для POST запроса парсим каждое поле, запиысывая его в отдельные переменные
+    request_data = request.get_json() # вытаскиваем в переменную наш запрос
+    friends = request_data["friends"] # отдельно достаём список друзей
+    guid = uuid.uuid1() # рандомный uuid
+    debitBalance = request_data["debitBalance"] # баланс1
+    creditBalance = request_data["creditBalance"] # баланс2
+    phone = request_data ["phone"] # номер телефона
+    registered = request_data["registered"] # регистрация
+    is_active = bool(request_data) # состояние
+    # многие переменные тут НЕОБЯЗАТЕЛЬНЫ, но пригодятся в случае масштабирования кода. Код интерпретируется и неиспользуемые переменные не занимают память
+        
+    # собираем ответ из ключей и их значений
     response = {
          "id": id,
          "guid": guid,
@@ -30,12 +31,12 @@ def json_post(id):
          "registered": registered,
          "lastActive": time_now()
                 }
-    return jsonify(response)                    # возвращаем тело ответа в формате JSON
+    return jsonify(response) # возвращаем тело ответа в формате JSON
 
-# обработчик запросов по методам GET и POST и PATH /api/json/
+# обработчик запросов по методу GET и PATH /api/json/
 @app.route('/api/json/', methods=['GET'])
 def json_get():
-    action = request.args.get('id')
+    action = request.args.get('id') # добавляем параметр для запроса
     response = {"id": action,  # передаем id клиета из PATH
                 "guid": uuid.uuid1(),  # рандомный giud
                 "balance": random.randrange(9999),  # рандомное 4-значное число
